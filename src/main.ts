ff52647bf4334266ee158317ae7fa720db134c22
perla-al-haddad <perlaalhaddad@gmail.com>
2023-09-26 22:29:55 +0300

    added post tags and date

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './assets/main.css'
 
-import 'highlight.js/styles/an-old-hope.css';
+// import 'highlight.js/styles/an-old-hope.css';
+// import 'highlight.js/styles/base16/nebula.css';
+import 'highlight.js/styles/atom-one-dark.css';
+// import 'highlight.js/styles/base16/atlas.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,120 +1,173 @@
 <script lang="ts">
 import MarkdownIt from 'markdown-it';
-import { defineComponent, onBeforeMount, onMounted, ref } from 'vue';
 import hljs from 'highlight.js';
-import categoriesPosts from "../../public/db.json";
+import { defineComponent, onBeforeMount, ref } from 'vue';
 import { useRoute } from "vue-router";
+import categoriesPosts from "../data/db.json";
 
-console.log(categoriesPosts);
-
-const fetchPost = async (fileName: String | String[]) => {
-  const response = await fetch(`/posts/${fileName}.md`);
-  const md = new MarkdownIt();
-
-  const results = md.render(await response.text())
-  hljs.highlightAll();
-  return results;
+interface IPost {
+    index: string,
+    title: string,
+    tags: Array<string>,
+    dateCreated?: Date
 }
 
-export default defineComponent({
-  setup(this) {
-    const route = useRoute();
-    let index = ref<string>(route.params.index as string);
-    let articleHTML = ref<string>("");
+const fetchPostList = () => {
+    let results: Array<IPost> = [];
 
-    onBeforeMount(async () => {
-      let articleIndex = index;
-      let articleContent = await fetchPost(articleIndex.value);
-      articleHTML.value = articleContent;
-    });
-
-    return {
-      index,
-      articleHTML
+    for (const category in categoriesPosts) {
+        if (!Object.prototype.hasOwnProperty.call(categoriesPosts, category)) {
+            continue;
+        }
+        const posts: Array<IPost> = (categoriesPosts as any)[category];
+        posts.forEach((post) => {
+            results.push(post);
+        })
     }
-  },
 
-  data() {
-    return {
-      index: '',
-      articleHTML: '',
-      categoriesPosts: categoriesPosts
-    }
-  },
+    return results;
+}
 
-  async beforeRouteUpdate(to, from) {
-    let articleIndex = to.params.index as string;
-    let articleHTML = await fetchPost(articleIndex);
-    this.articleHTML = articleHTML;
-    this.index = articleIndex;
-  },
+const postList = fetchPostList();
 
-  updated() {
+const fetchPost = async (fileName: String | String[]) => {
+    const response = await fetch(`/posts/${fileName}.md`);
+    const md = new MarkdownIt();
+
+    const results = md.render(await response.text())
     hljs.highlightAll();
-  }
+    return results;
+}
+
+const fetchPostMetadata = (index: String): IPost => {
+    const postMetadata: IPost = postList.filter((el) => el.index == index)[0];
+
+    if (postMetadata.dateCreated) {
+        postMetadata.dateCreated = new Date(postMetadata.dateCreated);
+    }
+
+    return postMetadata;
+}
+
+
+export default defineComponent({
+    setup(this) {
+        const route = useRoute();
+        let index = ref<string>(route.params.index as string);
+        let postHTML = ref<string>("");
+        let postMetadata = ref<IPost>();
+
+        onBeforeMount(async () => {
+            let postIndex = index;
+            let postContent = await fetchPost(postIndex.value);
+            postMetadata.value = fetchPostMetadata(postIndex.value);
+            postHTML.value = postContent;
+        });
+
+        return {
+            index,
+            postHTML,
+            postMetadata
+        }
+    },
+
+    data() {
+        return {
+            postMetadata: null as unknown as IPost,
+            index: '',
+            postHTML: '',
+            categoriesPosts: categoriesPosts
+        }
+    },
+
+    async beforeRouteUpdate(to, _) {
+        const postIndex = to.params.index as string;
+        const postHTML = await fetchPost(postIndex);
+        const postMetadata = fetchPostMetadata(postIndex);
+
+        this.postMetadata = postMetadata;
+        this.postHTML = postHTML;
+        this.index = postIndex;
+    },
+
+    updated() {
+        hljs.highlightAll();
+    }
 
 });
 </script>
 
 <template>
-  <main>
-    <div class="container my-5">
-      <div class="row">
-        <div class="col col-md-2 mb-5 sticky-col navigation-container">
-          <h6 class="fs-8 mt-3 text-bold text-uppercase">
-            Navigation
-            {{ index }}
-          </h6>
-          <hr>
-          <ul class="navbar-nav list-unstyled">
-            <li v-for="(posts, category) in categoriesPosts" class="nav-item">
-              <h6 class="fs-8 text-bold text-gray">
-                {{ category }}
-              </h6>
-              <ul>
-                <li v-for="post in posts" class="nav-item">
-                  <!-- <a href="" class="nav-link text-gray-light">{{ post.title }}</a> -->
-                  <RouterLink :to="`/${post.index}`"
-                    :class="`nav-link text-gray-light ${(index == post.index) ? 'active' : ''}`">{{ post.title }}
-                  </RouterLink>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+    <main>
+        <div class="container my-5">
+            <div class="row">
+                <div class="col col-md-2 mb-5 sticky-col navigation-container">
+                    <h6 class="fs-8 mt-3 text-bold text-uppercase">
+                        Navigation
+                    </h6>
+                    <hr>
+                    <ul class="navbar-nav list-unstyled">
+                        <li v-for="(posts, category) in categoriesPosts" class="nav-item">
+                            <h6 class="fs-8 text-bold text-gray">
+                                {{ category }}
+                            </h6>
+                            <ul>
+                                <li v-for="post in posts" class="nav-item">
+                                    <RouterLink :to="`/${post.index}`"
+                                        :class="`nav-link text-gray-light ${(index == post.index) ? 'active' : ''}`">{{
+                                            post.title }}
+                                    </RouterLink>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
 
-        <div class="col col-md-8 px-md-6">
-          <section class="post-container" v-html="articleHTML"></section>
-        </div>
+                <div v-if="postMetadata" class="col col-md-8 px-md-6">
+                    <h1>{{ postMetadata.title }}</h1>
+                    <p class="fs-8 text-gray-light text-end" v-if="postMetadata.dateCreated">{{ postMetadata.dateCreated.toLocaleDateString() }}</p>
+                    <ul class="list-unstyled d-flex gap-2 align-items-center">
+                        <li class="fs-8 tag" v-if="postMetadata.tags" v-for="tag in postMetadata.tags">#{{ tag }}</li>
+                    </ul>
+                    <hr class="mb-5">
+                    <section class="post-container" v-html="postHTML"></section>
+                </div>
 
-        <div class="col col-md-2">
-          <h6 class="fs-8 text-bold text-uppercase">
-            Table of contents
-          </h6>
-          <hr>
-          <ul class="navbar-nav list-unstyled">
-            <li class="nav-item">
-              <a href="" class="nav-link text-gray-light">
-              </a>
-            </li>
-          </ul>
+                <div class="col col-md-2">
+                    <h6 class="fs-8 text-bold text-uppercase">
+                        Table of contents
+                    </h6>
+                    <hr>
+                    <ul class="navbar-nav list-unstyled">
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-gray-light">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </main>
+    </main>
 </template>
 
 <style scoped>
 .navigation-container {
-  height: calc(100vh);
-  overflow-y: scroll;
+    height: calc(100vh);
+    overflow-y: auto;
+}
+
+.tag {
+    background-color: var(--bl-dark-soft);
+    padding: 3px 7px;
+    border-radius: 15px;
+    color: var(--bl-blue);
 }
 
 @media (min-width: 768px) {
-  .sticky-col {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 0;
-  }
+    .sticky-col {
+        position: -webkit-sticky;
+        position: sticky;
+        top: 0;
+    }
 }
 </style>
