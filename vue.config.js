2808d0afa71ce90e841c5f5af264cbd7152fae73
perla-al-haddad <perlaalhaddad@gmail.com>
2023-09-30 18:18:44 +0300

    edited config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/Blog/'
-        : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: "/Blog/"
+})
