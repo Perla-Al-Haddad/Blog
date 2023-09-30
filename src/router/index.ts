5146d9fe8ce2617c060e7fca4516af72f9e5bde8
perla-al-haddad <perlaalhaddad@gmail.com>
2023-09-30 19:30:09 +0300

    deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,22 +8,22 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: `/${defaultIndex}`
+      redirect: { name: "home" }
     },
     {
-      path: '/:index',
+      path: '/Blog/:index',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/about',
+      path: '/Blog/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
-  ]
+  ],
 })
 
 export default router
