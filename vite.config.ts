302b181c67b781ab212e1a5da828fdb04a8302ec
perla-al-haddad <perlaalhaddad@gmail.com>
2023-10-01 22:40:09 +0300

    added new post

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/Blog/"
+  base: "/Blog/",
+  build: {
+    outDir: 'docs'
+  },
 })
